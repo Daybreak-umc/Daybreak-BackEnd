4fc75fe802a3f1efef6092598cd14c6806c9cc69
이재용 <korom777755@gmail.com>
2025-12-15 22:51:29 +0900

    [Feat] Inital Commit

---
+++ src/main/java/umc9th_hackathon/daybreak/DaybreakApplication.java
@@ -0,0 +1,13 @@
+package umc9th_hackathon.daybreak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaybreakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaybreakApplication.class, args);
+	}
+
+}
---
+++ src/test/java/umc9th_hackathon/daybreak/DaybreakApplicationTests.java
@@ -0,0 +1,13 @@
+package umc9th_hackathon.daybreak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaybreakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
