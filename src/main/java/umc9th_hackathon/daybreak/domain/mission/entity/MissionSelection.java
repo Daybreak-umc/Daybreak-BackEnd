606bae75894b2b6aaa52640b0e980622b4445671
이재용 <korom777755@gmail.com>
2025-12-17 16:17:57 +0900

    [Feat] Member,Mission 도메인 Entity 생성

---
+++ src/main/java/umc9th_hackathon/daybreak/domain/mission/entity/MissionSelection.java
@@ -0,0 +1,49 @@
+package umc9th_hackathon.daybreak.domain.mission.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import umc9th_hackathon.daybreak.domain.member.entity.Member;
+import umc9th_hackathon.daybreak.global.entity.BaseEntity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Table(name = "mission_selection")
+public class MissionSelection extends BaseEntity {
+
+    @Id
+    @Column(name = "mission_selection_id")
+    private Long missionSelectionId;
+
+    @Id
+    @Column(name = "member_id")
+    private Long memberId;
+
+    @Id
+    @Column(name = "category_id")
+    private Long categoryId;
+
+    @Column(name = "objective", length = 50, nullable = false)
+    private String objective;
+
+
+    // 연관 관계
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", insertable = false, updatable = false)
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id", insertable = false, updatable = false)
+    private Category category;
+
+    @OneToMany(mappedBy = "missionSelection", cascade = CascadeType.ALL)
+    private List<Mission> memberMissions = new ArrayList<>();
+
+    @OneToMany(mappedBy = "missionSelection", cascade = CascadeType.ALL)
+    private List<Plan> plans = new ArrayList<>();
+}
--- src/main/java/umc9th_hackathon/daybreak/global/entity/BaseEntity.java
+++ src/main/java/umc9th_hackathon/daybreak/global/entity/BaseEntity.java
@@ -16,10 +16,10 @@ import java.time.LocalDateTime;
 public class BaseEntity {
 
     @CreatedDate
-    @Column(name = "create_time", nullable = false)
+    @Column(name = "created_at", nullable = false)
     private LocalDateTime createTime;
 
     @LastModifiedDate
-    @Column(name = "update_time", nullable = false)
+    @Column(name = "updated_at", nullable = false)
     private LocalDateTime updateTime;
 }
