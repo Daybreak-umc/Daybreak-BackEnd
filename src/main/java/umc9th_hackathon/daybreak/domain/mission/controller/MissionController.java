6770afb908b660593225793f0fe621e689a42d83
koreanpasta <hg2374@daum.net>
2025-12-19 11:14:22 +0900

    목표 달성 처리 api 구현

--- src/main/java/umc9th_hackathon/daybreak/domain/mission/controller/MissionController.java
+++ src/main/java/umc9th_hackathon/daybreak/domain/mission/controller/MissionController.java
@@ -3,8 +3,12 @@ package umc9th_hackathon.daybreak.domain.mission.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import umc9th_hackathon.daybreak.domain.mission.dto.res.MissionResponse;
+import umc9th_hackathon.daybreak.domain.mission.exception.MissionSuccessCode;
+import umc9th_hackathon.daybreak.domain.mission.service.MissionCommandService;
 import umc9th_hackathon.daybreak.domain.mission.service.MissionQueryService;
 import umc9th_hackathon.daybreak.global.apiPayload.ApiResponse;
 import umc9th_hackathon.daybreak.global.apiPayload.code.GeneralSuccessCode;
@@ -14,6 +18,7 @@ import umc9th_hackathon.daybreak.global.apiPayload.code.GeneralSuccessCode;
 public class MissionController {
 
     private final MissionQueryService missionQueryService;
+    private final MissionCommandService missionCommandService;
 
     @GetMapping("/api/v1/missions")
     public ApiResponse<MissionResponse.MissionGroupListDto> getGroupMissions(Authentication authentication) {
@@ -22,6 +27,17 @@ public class MissionController {
 
         return ApiResponse.onSuccess(
                 GeneralSuccessCode.REQUEST_OK,missionQueryService.getGroupMissions(email)
+        );
+    }
+
+    @PatchMapping("/api/v1/missions/complete")
+    public ApiResponse<MissionResponse.MissionCompleteDto> completeMission(Authentication authentication,
+           @RequestParam(value="missionId") Long missionId
+    ) {
+        String email = authentication.getName();
+
+        return ApiResponse.onSuccess(
+                MissionSuccessCode.SUCCESS_COMPLETE, missionCommandService.patchMissionComplete(missionId,email)
         );
     }
 }
