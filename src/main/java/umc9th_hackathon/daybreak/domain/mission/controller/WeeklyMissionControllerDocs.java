dfe1e96894954699e32fbb65b51e02cbf89587fc
최종건 <jong-geon@jong-geonui-MacBookAir.local>
2025-12-20 18:09:03 +0900

    [Docs] 1주일 미션 생성 api 설명 자세히 추가

--- src/main/java/umc9th_hackathon/daybreak/domain/mission/controller/WeeklyMissionControllerDocs.java
+++ src/main/java/umc9th_hackathon/daybreak/domain/mission/controller/WeeklyMissionControllerDocs.java
@@ -14,7 +14,9 @@ public interface WeeklyMissionControllerDocs {
 
     @Operation(
             summary = "미션 생성",
-            description = "사용자가 입력한 카테고리와 목표를 바탕으로 AI가 1주일치 미션을 생성합니다."
+            description = "사용자가 입력한 카테고리와 목표를 바탕으로 AI가 1주일치 미션을 생성합니다." +
+                    "category엔 건강, 학업, 커리어..... 중 하나를 입력하고 " +
+                    "goal엔 '나는 ~~~한 사람이 되어있을 거에요'에서 ~~~를 입력하면 된다."
     )
     @PostMapping("/weekly")
     ApiResponse<WeeklyMissionCreateResponse> createWeeklyMissions(
